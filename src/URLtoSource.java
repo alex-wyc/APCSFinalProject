560989bf0ad0ee8885b86b131e5f00bf0b173623
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-12-28 19:40:37 -0500

    updated

--- src/URLtoSource.java
+++ src/URLtoSource.java
@@ -14,11 +14,14 @@ public class URLtoSource {
 	ArrayList<String> paragraphs = new ArrayList<String>();
 
 	// Reg ex stuff for finding head and body
-	Pattern headPatS = Pattern.compile("<head.*>", Pattern.CASE_INSENSITIVE);
-	Pattern headPatE = Pattern.compile("</head>", Pattern.CASE_INSENSITIVE);
-	Pattern bodyPat = Pattern.compile("<body.*>.*</body>", Pattern.CASE_INSENSITIVE);
-	Pattern paragraphPat = Pattern.compile("<p.*>.*</p>", Pattern.CASE_INSENSITIVE);
-	Pattern titlePat = Pattern.compile("<title.*>.*</title>", Pattern.CASE_INSENSITIVE);
+	Pattern headPatS = Pattern.compile("\\<head*\\>", Pattern.CASE_INSENSITIVE);
+	Pattern headPatE = Pattern.compile("\\</head\\>", Pattern.CASE_INSENSITIVE);
+	Pattern bodyPatS = Pattern.compile("\\<body*\\>", Pattern.CASE_INSENSITIVE);
+	Pattern bodyPatE = Pattern.compile("\\</body\\>", Pattern.CASE_INSENSITIVE);
+	Pattern titlePatS = Pattern.compile("\\<title*\\>", Pattern.CASE_INSENSITIVE);
+	Pattern titlePatE = Pattern.compile("\\</title\\>", Pattern.CASE_INSENSITIVE);
+	Pattern paragraphPatS = Pattern.compile("\\<p*\\>", Pattern.CASE_INSENSITIVE);
+	Pattern paragraphPatE = Pattern.compile("\\</p\\>", Pattern.CASE_INSENSITIVE);
 	
 	// Constructors
 	public URLtoSource(String site) {
@@ -32,7 +35,7 @@ public class URLtoSource {
 			String inputLine;
 
 			while ((inputLine = br.readLine()) != null) {
-				source = source + inputLine;
+				source = source + inputLine + "\n";
 			}
 
 			br.close();
@@ -40,33 +43,60 @@ public class URLtoSource {
 			e.printStackTrace();
 		}
 
-		System.out.println(source);
+		//System.out.println(source);
 
 		Matcher headSFinder = headPatS.matcher(source);
-		start = headSFinder.end();
 
-		System.out.println("Head1 found");
+		if (headSFinder.find()) {
+			start = headSFinder.end();
+		}
 
 		Matcher headEFinder = headPatE.matcher(source);
-		end = headEFinder.start();
+
+		if (headEFinder.find()) {
+			end = headEFinder.start();
+		}
 
 		head = source.substring(start, end);
 
-		System.out.println("Head found");
+		start = end = 0;
 
-		Matcher bodyFinder = bodyPat.matcher(source);
-		body = bodyFinder.group();
-
-		System.out.println("Body found");
-
-		Matcher paragraphFinder = paragraphPat.matcher(body);
-
-		while (paragraphFinder.find()) {
-			paragraphs.add(paragraphFinder.group());
+		Matcher bodySFinder = bodyPatS.matcher(source);
+		
+		if (bodySFinder.find()) {
+			start = bodySFinder.end();
 		}
 
-		Matcher titleFinder = titlePat.matcher(head);
-		title = titleFinder.group();
+		Matcher bodyEFinder = bodyPatE.matcher(source);
+
+		if (bodyEFinder.find()) {
+			end = bodyEFinder.start();
+		}
+
+		body = source.substring(start,end);
+
+		start = end = 0;
+
+		Matcher titleSFinder = titlePatS.matcher(head);
+
+		if (titleSFinder.find()) {
+			start = titleSFinder.end();
+		}
+
+		Matcher titleEFinder = titlePatE.matcher(head);
+
+		if (titleEFinder.find()) {
+			end = titleEFinder.start();
+		}
+
+		title = head.substring(start,end);
+
+		Matcher paragraphSFinder = paragraphPatS.matcher(body);
+		Matcher paragraphEFinder = paragraphPatE.matcher(body);
+
+		while (paragraphSFinder.find() && paragraphEFinder.find()) {
+			paragraphs.add(body.substring(paragraphSFinder.end(), paragraphEFinder.start()));
+		}
 	}
 
 	// Methods
