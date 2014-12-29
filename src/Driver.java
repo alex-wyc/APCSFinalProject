836647b68c98c5e1991682e0661d869cacaf8fe8
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-12-28 23:40:53 -0500

    updated

--- src/Driver.java
+++ src/Driver.java
@@ -2,20 +2,21 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		URLtoSource test = new URLtoSource("http://marge.stuy.edu/~brandon.lin/"); // testing url
+		URLtoSource test = new URLtoSource("http://janus-chronicles.github.io/projectLucifer/"); // testing url
 
-		System.out.println(test.getSource());
+		//System.out.println(test.getSource());
 
-		System.out.println(test.getBody());
+		//System.out.println(test.getBody());
 
 		//System.out.println(test.getHead());
 
 		//System.out.println(test.getTitle());
 
 		//String[] ps = test.getParagraphs();
-
-		//for (String p : ps) {
-		//	System.out.println(p);
-		//}
+/*
+		for (String p : ps) {
+			System.out.println(p);
+		}
+*/
 	}
 }
