e5e8c678da0ab9491fa5d20deb9c79f37995c712
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-01-06 21:52:22 -0500

    Update ListFormatter.java

--- src/ListFormatter.java
+++ src/ListFormatter.java
@@ -4,7 +4,11 @@ public class ListFormatter {
 	
 	// Instance variables
 
-	ArrayList listEls = new ArrayList();
+	ArrayList<String> listEls = new ArrayList<String>();
+
+	// Regex stuff
+	Pattern listElPatS = Pattern.compile("\\<li.*?\\>|\\<ul.*?\\>|\\<ol.*?\\>", Pattern.CASE_INSENSITIVE);
+	Pattern listElPatE = Pattern.compile("\\</li\\>|\\</ul\\>|\\</ol\\>", Pattern.CASE_INSENSITIVE);
 
 	// Constructors
 
@@ -16,11 +20,31 @@ public class ListFormatter {
 
 	}
 
-	private ArrayList<String> orderedHandler(String html) {
+	private ArrayList<String> orderedHandeler(String html, int index) {
+		String preamble = "";
+		for (int i = 0 ; i < index ; i++) {
+			preamble = preamble + "\t";
+		}
 
+		ArrayList<String> out = new ArrayList<String>();
+		
+		int counter = 1;
+
+		Matcher listElPatFinderS = listElPatS.matcher(html);
+		Matcher listElPatFinderE = listElPatE.matcher(html);
+
+		boolean elFind = listElPatFinderS.find() && listElPatFinderE.find();
+
+		while (elFind) {
+
+		}
 	}
 
-	private ArrayList<String> unorderedHandler(String html) {
-
+	private ArrayList<String> unOrderedHandeler(String html, int index) {
+		String preamble = "";
+		for (int i = 0 ; i < index ; i++) {
+			preamble = preamble + "\t";
+		}
+		preamble = preamble + "> ";
 	}
 }
