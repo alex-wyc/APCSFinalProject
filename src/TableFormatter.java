e5c84e9d74ec1f21a9f067f5d483e9758bd7ab2e
daltonwu <dalton.z.wu@gmail.com>
2015-01-09 09:01:56 -0500

    Updated

--- src/TableFormatter.java
+++ src/TableFormatter.java
@@ -2,36 +2,36 @@ import java.util.regex.*;
 import java.util.ArrayList;
 
 public class TableFormatter {
-    // Take in currentParagraph from URLtoSource.java,
-    // Breaks stuff into 2-D ArrayList structure,
-    // Then spits it out as one string. I guess.
+	// Take in currentParagraph from URLtoSource.java,
+	// Breaks stuff into 2-D ArrayList structure,
+	// Then spits it out as one string. I guess.
+	
+	
+	// Instance variables
+	
+	String original;
+	String result;
+	
+	ArrayList< ArrayList<String> > grid = new ArrayList< ArrayList<String> >();
+	
+	Pattern trPatS = Pattern.compile("\\<tr.*?\\>", Pattern.CASE_INSENSITIVE);
+	Pattern trPatE = Pattern.compile("\\</tr\\>", Pattern.CASE_INSENSITIVE);
+	Pattern thPatS = Pattern.compile("\\<th.*?\\>", Pattern.CASE_INSENSITIVE);
+	Pattern thPatE = Pattern.compile("\\</th\\>", Pattern.CASE_INSENSITIVE);
+	Pattern tdPatS = Pattern.compile("\\<td.*?\\>", Pattern.CASE_INSENSITIVE);
+	Pattern tdPatE = Pattern.compile("\\</td\\>", Pattern.CASE_INSENSITIVE);
 
+	Matcher trSFinder, trEFinder;
+	Matcher thSFinder, thEFinder;
+	Matcher tdSFinder, tdEFinder;
 
-    // Instance variables
-    
-    String original;
-    String result;
-    
-    ArrayList< ArrayList<String> > grid = new ArrayList< ArrayList<String> >();
-    
-    Pattern trPatS = Pattern.compile("\\<tr.*?\\>", Pattern.CASE_INSENSITIVE);
-    Pattern trPatE = Pattern.compile("\\</tr\\>", Pattern.CASE_INSENSITIVE);
-    Pattern thPatS = Pattern.compile("\\<th.*?\\>", Pattern.CASE_INSENSITIVE);
-    Pattern thPatE = Pattern.compile("\\</th\\>", Pattern.CASE_INSENSITIVE);
-    Pattern tdPatS = Pattern.compile("\\<td.*?\\>", Pattern.CASE_INSENSITIVE);
-    Pattern tdPatE = Pattern.compile("\\</td\\>", Pattern.CASE_INSENSITIVE);
+	int thisRowWidth;
 
-    Matcher trSFinder, trEFinder;
-    Matcher thSFinder, thEFinder;
-    Matcher tdSFinder, tdEFinder;
-
-    int thisRowWidth;
-
-    // Constructor
-    
-    public TableFormatter(String s) {
-        original = s;
-        result = s;
+	// Constructor
+	
+	public TableFormatter(String s) {
+		original = s;
+		result = s;
 
 	trSFinder = trPatS.matcher(s);
 	trEFinder = trPatE.matcher(s);
@@ -39,25 +39,25 @@ public class TableFormatter {
 	thEFinder = thPatE.matcher(s);
 	tdSFinder = tdPatS.matcher(s);
 	tdEFinder = tdPatE.matcher(s);
-    }
-    
-    // Methods
-    
-    public String getResult() {
-	return result;
-    }
-    
-    void fillLists() {
+	}
 	
-    }
+	// Methods
+	
+	public String getResult() {
+	return result;
+	}
+	
+	void fillLists() {
+	
+	}
 
-    int tableWidth() {
-        int w = 0;
-        return w;
-    }
+	int tableWidth() {
+		int w = 0;
+		return w;
+	}
 
-    void doStuff() {
+	void doStuff() {
 	this.fillLists();
 	
-    }
+	}
 }
