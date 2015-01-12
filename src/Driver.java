7b93cf0938e99ce9951aba29fbfb40834af24f99
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-01-12 08:59:39 -0500

    updated

--- src/Driver.java
+++ src/Driver.java
@@ -10,7 +10,7 @@ public class Driver {
 
 		URLtoSource site = null;
 		
-		System.out.println("Welcom to <Insert Name> Browser, where would you like to go (URL)?");
+		System.out.println("Welcom to Athenian Rabbit Browser, where would you like to go (URL)?");
 		String URLStr = sc.nextLine();
 
 		try {
