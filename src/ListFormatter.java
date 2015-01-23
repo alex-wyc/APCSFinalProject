aa8b3b06f18ce0eeac3af3bc123949d94259128a
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-01-22 21:58:17 -0500

    updated

--- src/ListFormatter.java
+++ src/ListFormatter.java
@@ -11,6 +11,9 @@ public class ListFormatter {
 	Pattern listElPatS = Pattern.compile("\\<li.*?\\>|\\<ul.*?\\>|\\<ol.*?\\>", Pattern.CASE_INSENSITIVE);
 	Pattern listElPatE = Pattern.compile("\\</li\\>|\\</ul\\>|\\</ol\\>", Pattern.CASE_INSENSITIVE);
 
+	Pattern sublistPatS = Pattern.compile("\\<ul.*?\\>|\\<ol.*?\\>", Pattern.CASE_INSENSITIVE);
+	Pattern sublistPatE = Pattern.compile("\\</ul\\>|\\</ol\\>", Pattern.CASE_INSENSITIVE);
+
 	// Constructors
 
 	public ListFormatter(String html, boolean ordered) {
@@ -29,7 +32,7 @@ public class ListFormatter {
 			out = out + listEls.get(i) + "\n";
 		}
 		
-		return removeAllElse(out);
+		return out;
 	}
 
 	private ArrayList<String> orderedHandler(String html, int index) {
@@ -83,8 +86,38 @@ public class ListFormatter {
 
 			else {
 				// If this is a normal string
+				// stupid wikipedia formatting... actually has to check for sublists within <li> -_-
+				/* Failed attempt at fixing wikipedia formatting... causes heap space error for some reason
+				int start = listElPatFinderS.end();
+
+				while (!(listElPatFinderE.group().substring(2,4).equals("li"))) {
+					// i hate wikipedia...
+					listElPatFinderS.find();
+					listElPatFinderE.find();
+				}
+
+				String content = html.substring(start, listElPatFinderE.start());
+				Matcher containsSublist = sublistPatS.matcher(content);
+				Matcher sublistEnd = sublistPatE.matcher(content);
+
+				ArrayList<String> sublist = null;
+
+				if (containsSublist.find()) {
+					sublistEnd.find();
+					String furtherHTML = content.substring(containsSublist.end(), sublistEnd.start());
+					content = content.substring(0, containsSublist.start());
+					if (containsSublist.group().substring(1,3).equals("ul")) {
+						sublist = unOrderedHandler(furtherHTML, index + 1);
+					}
+					else {
+						sublist = orderedHandler(furtherHTML, index + 1);
+					}
+				}
+				*/
 				String content = html.substring(listElPatFinderS.end(), listElPatFinderE.start());
+				content = removeAllElse(content);
 				out.add(preamble + counter + ". " + content);
+				//out.addAll(sublist);
 				counter++;
 			}
 
@@ -95,12 +128,26 @@ public class ListFormatter {
 	}
 
 	private ArrayList<String> unOrderedHandler(String html, int index) {
-//		System.out.println(html);
+		System.out.println(html);
 		String preamble = "";
 		for (int i = 0 ; i < index ; i++) {
 			preamble = preamble + "\t";
 		}
 		preamble = preamble + "\u2022";
+		switch(index % 4) {
+			case 0:
+				preamble = "\u2022"; // black bullet
+				break;
+			case 1:
+				preamble = "\u25e6"; // hollow bullet
+				break;
+			case 2:
+				preamble = "\u25AA"; // square bullet
+				break;
+			case 3:
+				preamble = ">. "; // this
+				break;
+		}
 
 		ArrayList<String> out = new ArrayList<String>();
 
@@ -153,12 +200,51 @@ public class ListFormatter {
 				//</li>
 				//</ul>
 				//HOW TO DEAL WITH THIS?!?!
+				// Below is a failed attempt
+
 				String content = html.substring(listElPatFinderS.end(), listElPatFinderE.start());
+				content = removeAllElse(content);
 				out.add(preamble + " " + content);
+				/*
+
+				int start = listElPatFinderS.end();
+
+				boolean found = true;
+
+				while (found) {
+					// i hate wikipedia...
+					if (listElPatFinderE.group().substring(2,4).equals("li")) {
+						break;
+					}
+
+					found = listElPatFinderS.find() && listElPatFinderE.find();
+				}
+
+				String content = html.substring(start, listElPatFinderE.start());
+				Matcher containsSublist = sublistPatS.matcher(content);
+				Matcher sublistEnd = sublistPatE.matcher(content);
+
+				ArrayList<String> sublist = null;
+
+				if (containsSublist.find() && sublistEnd.find()) {
+					String furtherHTML = content.substring(containsSublist.end(), sublistEnd.start());
+					content = content.substring(0, containsSublist.start());
+					if (containsSublist.group().substring(1,3).equals("ul")) {
+						sublist = unOrderedHandler(furtherHTML, index + 1);
+					}
+					else {
+						sublist = orderedHandler(furtherHTML, index + 1);
+					}
+				}
+
+				out.add(preamble + content);
+				if (sublist != null) {
+					out.addAll(sublist);
+				}
+*/
 			}
 
 			elFind = listElPatFinderS.find() && listElPatFinderE.find();
-
 		}
 
 		return out;
