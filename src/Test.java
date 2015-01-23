f813c9d6fa8d7f0db77f3d73349a94df2f6b4313
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-01-23 00:36:26 -0500

    cleaned up repo

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-import java.util.regex.*;
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("$".length());
-	}
-}
--- src/TestSplit.java
+++
@@ -1,9 +0,0 @@
-public class TestSplit {
-	public static void main(String[] args) {
-		String str = "<td>a</td>,b,c,d,e,f,,g";
-		String[] splitArray = str.split(",");
-		for(String s : splitArray) {
-			System.out.print(s + " ");
-		}
-	}
-}
